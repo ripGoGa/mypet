66fcb90f4bd74a0eb97e7dabcddef2e182cc1b71
OboIgor <nanogranny@yandex.ru>
2025-09-29 14:33:10 +0500

    add workouts route and template with table

--- app/main.py
+++ app/main.py
@@ -8,7 +8,18 @@ app = FastAPI(title="Bike Tracker")
 
 templates = Jinja2Templates(directory='app/templates')
 
+workouts = [
+    {"date": "2025-09-20", "type": "Велотренировка", "duration": 90, "distance": 45},
+    {"date": "2025-09-22", "type": "Интервалы", "duration": 60, "distance": 30},
+    {"date": "2025-09-24", "type": "Легкая", "duration": 120, "distance": 55},
+]
+
 
 @app.get('/', response_class=HTMLResponse)
 async def hello_root(request: Request):
     return templates.TemplateResponse('index.html', {'request': request, 'user': 'Спортсмен'})
+
+
+@app.get('/workouts', response_class=HTMLResponse)
+async def list_workouts(request: Request):
+    return templates.TemplateResponse('workouts.html', {'request': request, 'workouts': workouts})
