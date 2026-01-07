2f8d4cc2e3a6ed2874dfdc1323ce9a4f1f656ae0
ripGoGa <nanogranny@yandex.ru>
2026-01-07 16:55:14 +0800

    feat(db): implement AthleteProfile and establish 1-to-1 relationship -Created AthleteProfile table to store dynamic fitness metrics separately from core user data. -Linked AthleteProfile to UserProfile using a shared primary key (Foreign Key). -Updated UserProfile to include a relationship back to AthleteProfile.

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime, timedelta
 from pathlib import Path
-from typing import Optional, List
+from typing import Optional, List, Text
 from sqlmodel import SQLModel, Field, Relationship
 
 
@@ -34,19 +34,34 @@ class Workout(SQLModel, table=True):
 
 class UserProfile(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True, default=None)
+    email_address: str
     name: str
     birth_date: Optional[date]
     height_cm: Optional[int]
-    weight_kg: float
-    ftp: int
     updated_at: datetime = Field(default_factory=datetime.now)
     messages: List['ChatMessage'] = Relationship(back_populates='user')
+    athlete_info: Optional['AthleteProfile'] = Relationship(back_populates='user')
+
+
+class AthleteProfile(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, foreign_key='userprofile.id')
+    weight_kg: Optional[float]
+    current_FTP: Optional[int]
+    limitations: Optional[Text]
+    equipment_tracking: Optional[Text]  # какие устройства есть у пользователя
+    weekly_hours: int  # Сколько времени есть на тренировки в неделю
+    usual_stats: Optional[Text]  # какие показатели у него обычно
+    weekly_routine: Optional[Text]  # Как обычно тренируется пользователь
+    available_time: Optional[float]  # сколько времени хочет тратить на тренировки
+    gear: Optional[Text]  # какое оборудование есть у пользователя( велосипед, станки..)
+    environment_location: Optional[Text]  # окружение, локация
+    user: Optional['UserProfile'] = Relationship(back_populates='athlete_info')
 
 
 class ChatMessage(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True, default=None)
     user_id: int = Field(foreign_key='userprofile.id')
-    role: str   # user или assistant
+    role: str  # user или assistant
     content: str
     created_at: datetime = Field(default_factory=datetime.now)
     user: Optional['UserProfile'] = Relationship(back_populates='messages')
